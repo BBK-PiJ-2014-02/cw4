a6c7b644cfedaf17995baa24bdc44451b573ef76
vhorta73 <diasfonseca@live.com>
2015-02-27 20:41:44 +0000

    PastMeeting unit tests for all new and extended methods. Cleaning up tabs to four spaces.

--- cw4/src/unitTests/TestPastMeeting.java
+++ cw4/src/unitTests/TestPastMeeting.java
@@ -18,80 +18,103 @@ import contactManager.PastMeetingImpl;
 /**
  * Unit test for the PastMeeting class implementation.
  * 
- * Please note that this class extends from the Meeting class
- * to which there are already unit tests being done to most methods.
- * 
- * This unit tests aims to test the new not yet tested methods, thus assuming
- * all the Meeting functionality is already covered by the TestMeeting class
- * and followed instructions as per the designed Meeting interface.
+ * Testing also Meeting implementations that may have been overridden 
+ * on this PastMeeting implementation.
  * 
  * @author Vasco
  *
  */
 public class TestPastMeeting {
-	/**
-	 * The past meeting implementation class object to test with.
-	 */
-	private PastMeeting pastMeeting;
-	
-	/**
-	 * PastMeeting's Id.
-	 */
-	private int ID = 123;
-	
-	/**
-	 * PastMeeting's date.
-	 */
-	private Calendar DATE;
-	
-	/**
-	 * PastMeeting's list of contacts.
-	 */
-	private Set<Contact> contactList;
-	
-	/**
-	 * PastMeeting's notes.
-	 */
-	private String NOTES = "Some notes";
+    /**
+     * The past meeting implementation class object to test with.
+     */
+    private PastMeeting pastMeeting;
+    
+    /**
+     * PastMeeting's Id.
+     */
+    private int ID = 123;
+    
+    /**
+     * PastMeeting's date.
+     */
+    private Calendar DATE;
+    
+    /**
+     * PastMeeting's list of contacts.
+     */
+    private Set<Contact> contactList;
+    
+    /**
+     * PastMeeting's notes.
+     */
+    private String NOTES = "Some notes";
 
-	/**
-	 * Needed initialisations before each test.
-	 * 
-	 * @throws Exception 
-	 */
-	@Before
-	public void before() {
-		// Initialise DATE
-		DATE = new GregorianCalendar();
-		DATE.set(Calendar.YEAR, Calendar.MONTH, Calendar.DATE, Calendar.HOUR_OF_DAY, Calendar.MINUTE, Calendar.SECOND);
+    /**
+     * Needed initialisations before each test.
+     * 
+     * @throws Exception 
+     */
+    @Before
+    public void before() {
+        // Initialise DATE
+        DATE = new GregorianCalendar();
+        DATE.set(Calendar.YEAR, Calendar.MONTH, Calendar.DATE, Calendar.HOUR_OF_DAY, Calendar.MINUTE, Calendar.SECOND);
 
-		// Add some random contacts expected to be returned later
-		contactList = new HashSet<Contact>();
-		contactList.add(new ContactImpl(0, "First contact", null));
-		contactList.add(new ContactImpl(1, "Second contact", NOTES));
-		
-		// Initialise meeting
-		pastMeeting = new PastMeetingImpl(ID, DATE, contactList, NOTES);
-	}
-	
-	/**
-	 * Testing null notes.
-	 */
-	@Test
-	public void testNullNotes() {
-		PastMeeting pm = new PastMeetingImpl(ID, DATE, contactList, null);
-		String foundNotes = pm.getNotes();
-		String expectedNotes = "";
-		assertEquals(expectedNotes,foundNotes);
-	}
+        // Add some random contacts expected to be returned later
+        contactList = new HashSet<Contact>();
+        contactList.add(new ContactImpl(0, "First contact", null));
+        contactList.add(new ContactImpl(1, "Second contact", NOTES));
 
-	/**
-	 * Testing getNotes().
-	 */
-	@Test
-	public void testNotes() {
-		String foundNotes = pastMeeting.getNotes();
-		assertEquals(NOTES, foundNotes);
-	}
-	
+        // Initialise meeting
+        pastMeeting = new PastMeetingImpl(ID, DATE, contactList, NOTES);
+    }
+    
+    /**
+     * Testing getId.
+     */
+    @Test
+    public void testGetId() {
+        int foundId = pastMeeting.getId();
+        assertEquals(ID,foundId);
+    }
+
+    /**
+     * Testing getContacts.
+     */
+    @Test
+    public void testGetContacts() {
+        Set<Contact> contactsFound = pastMeeting.getContacts();
+        assertEquals(contactList,contactsFound);
+    }
+    
+    /**
+     * Testing getDate.
+     */
+    @Test
+    public void testGetDate() {
+        Calendar dateFound = pastMeeting.getDate();
+        assertEquals(DATE,dateFound);
+    }
+
+    /**
+     * Testing null notes.
+     */
+    @Test
+    public void testNullNotes() {
+        PastMeeting pm = new PastMeetingImpl(ID, DATE, contactList, null);
+        String foundNotes = pm.getNotes();
+        String expectedNotes = "";
+        assertEquals(expectedNotes,foundNotes);
+    }
+
+    /**
+     * Testing getNotes().
+     */
+    @Test
+    public void testNotes() {
+        String foundNotes = pastMeeting.getNotes();
+        assertEquals(NOTES, foundNotes);
+    }
+    
 }
