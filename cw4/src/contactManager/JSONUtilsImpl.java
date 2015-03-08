10c3dfb32f50ccc9c0d95d2416d127ebd3233b06
BBK-PiJ-2014-02 <vdiasf01@dcs.bbk.ac.uk>
2015-03-08 22:49:31 +0000

    JSONUtils Implementation - Added new method conversion to Calendar from JSON - Added new method conversion to JSON from Calendar - Fixed the date conversions where required.

--- cw4/src/contactManager/JSONUtilsImpl.java
+++ cw4/src/contactManager/JSONUtilsImpl.java
@@ -27,9 +27,13 @@ public class JSONUtilsImpl implements JSONUtils {
     @Override
     public JSONObject toJSONObject(Contact contact) {
         JSONObject jo = new JSONObject();
+        // If nothing is returned, keep integrity by returning
+        // an empty JSONObject.
+        if ( contact == null ) return jo;
+
         jo.put("id", contact.getId());
-        jo.put("name", contact.getName());
-        jo.put("notes", contact.getNotes());
+        jo.put("name", contact.getName().toString());
+        jo.put("notes", contact.getNotes().toString());
         return jo;
     }
 
@@ -40,15 +44,19 @@ public class JSONUtilsImpl implements JSONUtils {
     @Override
     public JSONObject toJSONObject(Meeting meeting) {
         JSONObject jo = new JSONObject();
+
+        // If meeting is null, keep integrity by returning and empty JSON Object.
+        if ( meeting == null ) return jo;
+
         jo.put("id", meeting.getId());
-        jo.put("date", meeting.getDate());
-        
+        jo.put("date", toJSONObject(meeting.getDate()));
+
         JSONArray contactsJO = new JSONArray();
         for( Contact c : meeting.getContacts() ) {
             JSONObject contactJO = new JSONObject();
             contactJO.put("id", c.getId());
-            contactJO.put("name", c.getName());
-            contactJO.put("notes", c.getNotes());
+            contactJO.put("name", c.getName().toString());
+            contactJO.put("notes", c.getNotes().toString());
             contactsJO.add(contactJO);
         }
 
@@ -75,13 +83,16 @@ public class JSONUtilsImpl implements JSONUtils {
     @SuppressWarnings("unchecked")
     @Override
     public Meeting toMeeting(JSONObject jObject) {
+        // If jObject is null, return null
+        if ( jObject == null ) return null;
+
         // Read the meeting id
         Integer id      = Integer.valueOf(jObject.get("id").toString());
-        
+
         // Load the date.
-        Calendar date   = new GregorianCalendar();
-                 date = (Calendar) jObject.get("date");
-                 
+        JSONObject jDate = (JSONObject) jObject.get("date");
+        Calendar date = toCalendar(jDate);
+
         // Load all set contacts.
         Set<Contact> contacts = new HashSet<Contact>();
         JSONArray jArray = (JSONArray) jObject.get("contacts");
@@ -99,7 +110,59 @@ public class JSONUtilsImpl implements JSONUtils {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
         return meeting;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @SuppressWarnings({ "unchecked" })
+    @Override
+    public JSONObject toJSONObject(Calendar date) {
+        // Create the JSON object to be returned
+        JSONObject jo = new JSONObject();
+
+        // If not date was supplied, return the empty JSON Object
+        if ( date == null ) return jo;
+
+        Integer year   = date.get(Calendar.YEAR);
+        Integer month  = date.get(Calendar.MONTH);
+        Integer day    = date.get(Calendar.DAY_OF_MONTH);
+        Integer hour   = date.get(Calendar.HOUR_OF_DAY);
+        Integer minute = date.get(Calendar.MINUTE);
+        Integer second = date.get(Calendar.SECOND);
+
+        jo.put("year", year);
+        jo.put("month", month);
+        jo.put("day", day);
+        jo.put("hour", hour);
+        jo.put("minute", minute);
+        jo.put("second", second);
+
+        return jo;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Calendar toCalendar(JSONObject jObject) {
+        // Create a new instance of the Calendar to be returned.
+        Calendar date = new GregorianCalendar();
+
+        // If no jObject given, return date as is.
+        if ( jObject == null ) return date;
+
+        Integer year   = Integer.valueOf(jObject.get("year").toString());
+        Integer month  = Integer.valueOf(jObject.get("month").toString());
+        Integer day    = Integer.valueOf(jObject.get("day").toString());
+        Integer hour   = Integer.valueOf(jObject.get("hour").toString());
+        Integer minute = Integer.valueOf(jObject.get("minute").toString());
+        Integer second = Integer.valueOf(jObject.get("second").toString());
+
+        date.set(year, month, day, hour, minute, second);
+
+        return date;
     }
 }
