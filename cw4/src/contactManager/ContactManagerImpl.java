336e931df2d4e2afc0f546914302e2ff334a5a44
BBK-PiJ-2014-02 <vdiasf01@dcs.bbk.ac.uk>
2015-03-09 23:23:30 +0000

    Contact Manager: - Adding exception to addNewContact on null name.

--- cw4/src/contactManager/ContactManagerImpl.java
+++ cw4/src/contactManager/ContactManagerImpl.java
@@ -206,6 +206,7 @@ public class ContactManagerImpl implements ContactManager {
     @Override
     public void addNewContact(String name, String notes) {
         if ( notes == null ) throw new NullPointerException();
+        if ( name == null ) throw new NullPointerException();
         Contact contact = new ContactImpl(this.contactId+1, name, notes);
         this.contactId++;
         contactList.add(contact);
