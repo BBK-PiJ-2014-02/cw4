f75be68928dcb1eba4df414b4f2a0c78f0f6402e
BBK-PiJ-2014-02 <vdiasf01@dcs.bbk.ac.uk>
2015-03-18 23:28:00 +0000

    JSON Utils Interface Add FutureMeeting and PastMeeting conversions to and from JSON

--- cw4/src/contactManager/JSONUtils.java
+++ cw4/src/contactManager/JSONUtils.java
@@ -5,54 +5,70 @@ import java.util.Calendar;
 import org.json.simple.JSONObject;
 
 public interface JSONUtils {
-	/**
-	 * Given a Calendar date, returns the JSONObject for given
-	 * date on year, month, day, hour, minute, second and millisecond.
-	 * 
-	 * @param date Calendar date
-	 * @return JSONOnject
-	 */
-	JSONObject toJSONObject(Calendar date);
+    /**
+     * Given a Calendar date, returns the JSONObject for given
+     * date on year, month, day, hour, minute, second and millisecond.
+     * 
+     * @param date Calendar date
+     * @return JSONOnject
+     */
+    JSONObject toJSONObject(Calendar date);
 
-	/**
-	 * Given a Contact, returns the JSONObject for given Contact
-	 * in the JSONObject format, ready to be saved on the file.
-	 * 
-	 * @param contact to be converted
-	 * @return JSONObject
-	 */
-	JSONObject toJSONObject(Contact contact);
+    /**
+     * Given a Contact, returns the JSONObject for given Contact
+     * in the JSONObject format, ready to be saved on the file.
+     * 
+     * @param contact to be converted
+     * @return JSONObject
+     */
+    JSONObject toJSONObject(Contact contact);
 
-	/**
-	 * Given a Meeting, returns the JSONObject for given Meeting
-	 * in the JSONObject format, ready to be saved on the file.
-	 * 
-	 * @param meeting to be converted
-	 * @return JSONObject
-	 */
-	JSONObject toJSONObject(Meeting meeting);
+    /**
+     * Given a Meeting, returns the JSONObject for given Meeting
+     * in the JSONObject format, ready to be saved on the file.
+     * 
+     * @param meeting to be converted
+     * @return JSONObject
+     */
+    JSONObject toJSONObject(Meeting meeting);
 
-	/**
-	 * Convert a JSONObject into a Calendar object.
-	 * 
-	 * @param jObject of type Calendar
-	 * @return Calendar
-	 */
-	Calendar toCalendar(JSONObject jObject);
-	
-	/**
-	 * Convert a JSONObject into a Contact object.
-	 * 
-	 * @param jObject of type Contact
-	 * @return Contact
-	 */
-	Contact toContact(JSONObject jObject);
-	
-	/**
-	 * Convert a JSONObject into a Meeting object
-	 * 
-	 * @param jObject of type meeting
-	 * @return Meeting
-	 */
-	Meeting toMeeting(JSONObject jObject);
+    /**
+     * Convert a JSONObject into a Calendar object.
+     * 
+     * @param jObject of type Calendar
+     * @return Calendar
+     */
+    Calendar toCalendar(JSONObject jObject);
+    
+    /**
+     * Convert a JSONObject into a Contact object.
+     * 
+     * @param jObject of type Contact
+     * @return Contact
+     */
+    Contact toContact(JSONObject jObject);
+    
+    /**
+     * Convert a JSONObject into a Meeting object
+     * 
+     * @param jObject of type meeting
+     * @return Meeting
+     */
+    Meeting toMeeting(JSONObject jObject);
+
+    /**
+     * Convert a JSONObject into a PastMeeting object
+     * 
+     * @param jObject of type meeting
+     * @return PastMeeting
+     */
+    PastMeeting toPastMeeting(JSONObject jObject);
+
+    /**
+     * Convert a JSONObject into a FutureMeeting object
+     * 
+     * @param jObject of type meeting
+     * @return FutureMeeting
+     */
+    FutureMeeting toFutureMeeting(JSONObject jObject);
 }
