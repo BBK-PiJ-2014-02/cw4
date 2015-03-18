d4896ce04f2a546427a7e7019076411a6542f920
BBK-PiJ-2014-02 <vdiasf01@dcs.bbk.ac.uk>
2015-03-18 23:30:24 +0000

    JSON Utils Interface - No need to add extra methods to the interface. toMeeting() will do all.

--- cw4/src/contactManager/JSONUtils.java
+++ cw4/src/contactManager/JSONUtils.java
@@ -55,20 +55,4 @@ public interface JSONUtils {
      * @return Meeting
      */
     Meeting toMeeting(JSONObject jObject);
-
-    /**
-     * Convert a JSONObject into a PastMeeting object
-     * 
-     * @param jObject of type meeting
-     * @return PastMeeting
-     */
-    PastMeeting toPastMeeting(JSONObject jObject);
-
-    /**
-     * Convert a JSONObject into a FutureMeeting object
-     * 
-     * @param jObject of type meeting
-     * @return FutureMeeting
-     */
-    FutureMeeting toFutureMeeting(JSONObject jObject);
 }
